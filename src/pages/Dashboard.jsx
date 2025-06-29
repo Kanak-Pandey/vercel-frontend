7c85ef298939d6d83605aac18a039fa1dbed2452
Kanak-Pandey <kanakpandey302004@gmail.com>
2025-06-29 22:41:32 +0530

    balance feature

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import { AppBar } from "../components/AppBar";
 import { Balance } from "../components/Balance";
+import { Button } from "../components/Button";
 import { Users } from "../components/Users";
 
 
 export function Dashboard(){
+    const [balance,setBalance]=useState("");
+    useEffect(()=>{
+        const token=localStorage.getItem("token");
+        axios.get("https://vercel-backend-kanak-pandeys-projects.vercel.app/api/v1/account/balance", {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        }).then(response => {
+            setBalance(response.data.balance.toFixed(2));
+        })
+        .catch(error => {
+            console.error("Failed to fetch balance:", error);
+            setBalance("0.00");
+        });
+    },[balance])
     return <div>
         <AppBar/>
         <div className="m-8">
-            <Balance value={"10,000"}></Balance>
+            <Balance value={balance}></Balance>
             <Users></Users>
+            
 
         </div>
     </div>
