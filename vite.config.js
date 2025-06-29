f4af8d060cc4edf19f7ffff48b024b98bf232900
Kanak-Pandey <kanakpandey302004@gmail.com>
2025-06-29 18:49:43 +0530

    vite config altered

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    cssMinify: 'esbuild' // Force esbuild for CSS minification
+  },
+  optimizeDeps: {
+    exclude: ['lightningcss'] // Prevent LightningCSS issues
+  }
 })
